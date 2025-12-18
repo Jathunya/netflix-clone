c22b273b40234834c6bd6eb9594175c66621bd21
Oranut Suphantamat <Oranut_s@kkumail.com>
2025-12-18 10:06:46 +0700

    feat(create):create category

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Home from './pages/Home/Home'
+import { Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <Routes>
+        <Route path='/'  />
+      </Routes>
       <Home />
     </div>
   )
